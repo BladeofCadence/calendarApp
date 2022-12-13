83cc07442c3cb7a2b3d3c2e01f337e84471b8d93
bladeofcadence <bladeofcadence@gmail.com>
2022-12-12 22:44:27 -0600

    everything

---
+++ calendarApp/calendarcell.swift
@@ -0,0 +1,16 @@
+//
+//  calendarcell.swift
+//  calendarApp
+//
+//  Created by Kelli Norris on 12/12/22.
+//
+
+import Foundation
+import UIKit
+
+class CalendarCell: UICollectionViewCell{
+    
+    @IBOutlet weak var dayOfMonth: UILabel!
+}
+
+
